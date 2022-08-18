4f1d4e29dd6e7ef49e3b4f77ed0836e31e352cc3
meowkio <111453469+meowkio@users.noreply.github.com>
2022-08-18 15:54:22 +0100

    Initial Commit

---
+++ idk2/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  idk2
+//
+//  Created by Chloe Loca on 18/08/2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
